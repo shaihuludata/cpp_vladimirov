d29335c08c2e3203f6308918104545257d6e118d
Vyacheslav Korchagin <ming7f@outlook.com>
2025-05-02 18:52:59 +0700

    comment main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,22 @@
 
 using namespace std;
 
+/**
+ * \section prototypes.
+ */
 int f ();
+
+/**
+ * \section implementation.
+ */
+int f()
+{
+    FILE* pFile;
+    pFile = fopen("unexist.ent", "r");
+    if (pFile == NULL)
+        printf("Error opening file unexist.ent: %s\n", strerror(errno));
+    return 0;
+}
 
 /**
  * \section main.
@@ -25,11 +40,4 @@ int main()
     return 0;
 }
 
-int f ()
-{
-  FILE * pFile;
-  pFile = fopen ("unexist.ent","r");
-  if (pFile == NULL)
-    printf ("Error opening file unexist.ent: %s\n",strerror(errno));
-  return 0;
-}
+
