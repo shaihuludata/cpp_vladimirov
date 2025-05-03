0e453b64ea151e594878e08125f127c925c69f62
shaihuludata <sandworm1986@gmail.com>
2025-05-03 16:24:22 +0700

    первая лекция, второе задание про шаблонизатор

--- main.cpp
+++ main.cpp
@@ -1,68 +1,36 @@
 #include <iostream>
+#include <cassert>
 
-class string {
-    char * data;
-    size_t size;
-    size_t capacity;
+/* задача:
+ * написать функцию
+ * int replace_all для std::string
+ * что-то вроде шаблонизатора, получается
+ *
+ */
+using namespace std;
 
-public:
-    string (char * s) {
-        for(size = 0; s[size] != '\0'; size++) { }
-        capacity = size + 10;
-        data = (char *)malloc(capacity);
-        for (size = 0; s[size] != '\0'; size++) {
-            data[size] = s[size];
-        }
-    }
-
-    string (const string &s) {
-        size = s.size;
-        capacity = s.capacity;
-        data = (char *)malloc(capacity);
-        for (int i = 0; i <= size; i++) {
-            data[i] = s.data[i];
-            if (s.data[i] == '\0') {
-                break;
-            }
-        }
-    }
-
-    ~string () {
-        free(data);
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const string s) {
-        os << "my_string: " << s.data ;
-        return os;
-    }
-
-    string operator=(const string & s) {
-        free(data);
-        size = s.size;
-        capacity = s.capacity;
-        data = (char *)malloc(capacity);
-        for (int i = 0; i <= size; i++) {
-            data[i] = s.data[i];
-            if (s.data[i] == '\0') {
-                break;
-            }
-        }
-        std::cerr << "not implemented yet" << std::endl;
-        exit(1);
-    }
-};
+int replace_all(string & str, string & from, string & to);
 
 /**
  * \section main.
  */
 int main() {
-    string s((char*)"Hello World!");
-    string scopy(s);
-    string s1((char *)"Other text");
-    string s2 = s1;
-    std::cout << s << std::endl;
-    std::cout << scopy << std::endl;
-    std::cout << s1 << std::endl;
-    std::cout << s2 << std::endl;
+    string str("Hello %username! How are you doing, %username?");
+    std::cout << str << std::endl;
+    string from("%username");
+    string to("Denis, the Toxic");
+    int nrepl = replace_all(str, from, to);
+    std::cout << str << std::endl;
+    assert(nrepl == 2);
     return 0;
 }
+
+int replace_all(string & str, string & from, string & to) {
+    int nrepl = 0;
+    std::size_t npos;
+    while ((npos = str.find(from)) != std::string::npos) {
+        str.replace(npos, from.length(), to);
+        nrepl++;
+    }
+    return nrepl;
+};
