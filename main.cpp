1d950fb2622d64a1ad0f0118828bb015912e6608
shaihuludata <sandworm1986@gmail.com>
2025-05-03 13:56:41 +0700

    копирующий конструктор и оператор=

--- main.cpp
+++ main.cpp
@@ -12,8 +12,8 @@ class string {
     char * data;
     size_t size;
     size_t capacity;
-public:
 
+public:
     string (char * s) {
         size = strlen(s);
         capacity = size + 10;
@@ -21,27 +21,41 @@ public:
         strncpy(data, s, size);
     }
 
+    string (const string &s) {
+        size = s.size;
+        capacity = s.capacity;
+        data = (char *)malloc(capacity);
+        strncpy(data, s.data, size);
+    }
+
     ~string () {
-//        free(data);
+        free(data);
     }
 
     friend std::ostream& operator<<(std::ostream& os, const string s) {
         os << "my_string: " << s.data ;
         return os;
     }
-    // TODO copy assignment
-//    const string operator=() {
-//
-//    }
+
+    string operator=(const string & s) {
+        free(data);
+        size = s.size;
+        capacity = s.capacity;
+        data = (char *)malloc(capacity);
+        strncpy(data, s.data, s.size);
+        std::cerr << "not implemented yet" << std::endl;
+        exit(1);
+    }
 };
 
-int main()
-{
+int main() {
     string s((char*)"Hello World!");
-//    string scopy(s);
-//    string s1("Other text");
-//    s1 = s;
+    string scopy(s);
+    string s1((char *)"Other text");
+    string s2 = s1;
     std::cout << s << std::endl;
-
+    std::cout << scopy << std::endl;
+    std::cout << s1 << std::endl;
+    std::cout << s2 << std::endl;
     return 0;
 }
